b494f7abe2a003282266f41faedb091c14a765cf
Amagin0 <amaginyos.ask.given.u@gmail.com>
2021-12-30 00:13:42 +0900

    アプリ側完成

--- rspec-sample/config/routes.rb
+++ rspec-sample/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'food_enquetes/index'
+  get 'food_enquetes/show'
+  get 'food_enquetes/new'
+  get 'food_enquetes/edit'
+  get 'food_enquetes/create'
+  get 'food_enquetes/update'
+  get 'food_enquetes/destroy'
+  resources :gym_enquetes
+  resources :food_enquetes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
